a55e918b4fd7278b99f70e47d3f77672ef9cf14c
NEWUSER <frontendstack2002@gmail.com>
2022-11-25 21:49:43 +0700

    Add file

---
+++ Vaccine/Vaccine/DTO/VaccineDTO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaccine.DTO
+{
+    class VaccineDTO
+    {
+        private string MaDM;
+        private string HSD;
+        private string TenVC;
+        private string MaVC;
+
+        public string MaDM1
+        {
+            get { return MaDM; }
+            set { MaDM = value; }
+        }
+
+        public string MaVC1
+        {
+            get { return MaVC; }
+            set { MaVC = value; }
+        }
+
+        public string TenVC1
+        {
+            get { return TenVC; }
+            set { TenVC = value; }
+        }
+
+        public string HSD1
+        {
+            get { return HSD; }
+            set { HSD = value; }
+        }
+    }
+}
